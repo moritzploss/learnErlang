5a924e44c996feaf0b9d2f44cde6472cd73bd510
Moritz <moritz.ploss@gmail.com>
2020-12-14 13:22:52 +0100

    Refactoring

--- lyse/bikeShop/src/bike_gen_server.erl
+++ lyse/bikeShop/src/bike_gen_server.erl
@@ -2,8 +2,9 @@
 
 -behaviour(gen_server).
 
--export([init/1, start_link/0, order_bike/3, return_bike/2, get_bikes/1, close_shop/1]).
+-export([init/1, start_link/0]).
 -export([handle_call/3, handle_cast/2, terminate/2, handle_info/2, code_change/3]).
+-export([order_bike/3, return_bike/2, get_bikes/1, close_shop/1]).
 
 -record(bike, {
   model :: string(),
