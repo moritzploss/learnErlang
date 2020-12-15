67f94ba6f0ce5b628e33b6c8e3c9d5b13b9bc5d0
Moritz <moritz.ploss@gmail.com>
2020-12-15 12:10:35 +0100

    Improve typings

--- src/lyse/eventServer/server.erl
+++ src/lyse/eventServer/server.erl
@@ -3,20 +3,9 @@
 -export([start/0, start_link/0, stop/1, add_event/3, subscribe/1]).
 -export([init/1]).
 
--type event() :: {
-    owner,
-    pid(),
-    name,
-    string(),
-    pid,
-    pid()
-}.
-
 -type name() :: string().
 
 -type monitor_ref() :: any().
-
--type orddict(Key, Value) :: orddict(Key, Value).
 
 -record(event, {
     owner :: pid(),
@@ -25,19 +14,23 @@
 }).
 
 -record(state, {
-    clients = orddict:new() :: orddict(monitor_ref(), pid()),
-    events = orddict:new() :: orddict(name(), event())
+    clients = #{} :: #{monitor_ref() := pid()},
+    events = #{} :: #{name() := #event{}}
 }).
 
+-spec start() -> pid().
 start() ->
     spawn(?MODULE, init, [#state{}]).
 
+-spec start_link() -> pid().
 start_link() ->
     spawn_link(?MODULE, init, [#state{}]).
 
+-spec stop(Pid :: pid()) -> shutdown.
 stop(Pid) ->
     Pid ! shutdown.
 
+-spec subscribe(Pid :: pid()) -> {ok, monitor_ref()} | {error, atom()}.
 subscribe(Pid) ->
     Ref = erlang:monitor(process, whereis(?MODULE)),
     Pid ! {self(), Ref, {subscribe}},
@@ -49,6 +42,7 @@ subscribe(Pid) ->
     after 5000 -> {error, timeout}
     end.
 
+-spec add_event(Pid :: pid(), Name :: string(), Delay :: integer()) -> ok | {error, timeout}.
 add_event(Pid, Name, Delay) ->
     Ref = make_ref(),
     Pid ! {self(), Ref, {add, Name, Delay}},
@@ -58,27 +52,29 @@ add_event(Pid, Name, Delay) ->
     after 5000 -> {error, timeout}
     end.
 
+-spec init(State :: #state{}) -> ok.
 init(State = #state{}) ->
     loop(State).
 
+-spec loop(State :: #state{}) -> ok.
 loop(State = #state{}) ->
     receive
         {Client, MsgRef, {subscribe}} ->
             Ref = erlang:monitor(process, Client),
-            Clients = orddict:store(Ref, Client, State#state.clients),
+            Clients = maps:put(Ref, Client, State#state.clients),
             Client ! {MsgRef, ok},
             loop(State#state{clients = Clients});
         {Client, MsgRef, {add, Name, Delay}} ->
             EventPid = event:start_link(Name, Delay),
             Event = #event{owner = Client, name = Name, pid = EventPid},
-            Events = orddict:store(Name, Event, State#state.events),
+            Events = maps:put(Name, Event, State#state.events),
             Client ! {MsgRef, ok},
             loop(State#state{events = Events});
         {Client, MsgRef, {cancel, Name}} ->
-            case orddict:find(Name, State#state.events) of
+            case maps:find(Name, State#state.events) of
                 {ok, #event{pid = EventPid}} ->
                     ok = event:cancel(EventPid),
-                    Events = orddict:erase(Name, State#state.events),
+                    Events = map:put(Name, State#state.events),
                     Client ! {MsgRef, ok},
                     loop(State#state{events = Events});
                 error ->
@@ -86,13 +82,13 @@ loop(State = #state{}) ->
                     loop(State)
             end;
         {done, Name} ->
-            #event{owner = OwnerPid} = orddict:fetch(Name, State#state.events),
+            #event{owner = OwnerPid} = maps:get(Name, State#state.events),
             OwnerPid ! {done, Name},
-            Events = orddict:erase(Name, State#state.events),
+            Events = maps:remove(Name, State#state.events),
             loop(State#state{events = Events});
         shutdown ->
             ok;
         {'DOWN', Ref, process, _Pid, _Reason} ->
-            Clients = orddict:erase(Ref, State#state.clients),
+            Clients = maps:remove(Ref, State#state.clients),
             loop(State#state{clients = Clients})
     end.
--- src/sandbox/fileServer/client.erl
+++ src/sandbox/fileServer/client.erl
@@ -4,22 +4,21 @@
 
 ls(Server) ->
     Server ! {self(), list_dir},
-        receive
-      {Server, FileList} ->
-          FileList
+    receive
+        {Server, FileList} ->
+            FileList
     end.
 
 get_file(Server, FileName) ->
     Server ! {self(), {get_file, FileName}},
     receive
-      {Server, Content} ->
-          Content
+        {Server, Content} ->
+            Content
     end.
 
 put_file(Server, Content, FileName) ->
     Server ! {self(), {put_file, {Content, FileName}}},
     receive
-      {Server, ok} ->
-          ok
+        {Server, ok} ->
+            ok
     end.
-
